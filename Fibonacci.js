01dc4855e821bfdafd94d4ff722d719f50a153de
Patryk <patrykkonior4@gmail.com>
2023-12-17 11:16:48 +0100

    Iteration vs Recursion

---
+++ Fibonacci.js
@@ -0,0 +1,23 @@
+// Interation
+
+function iterativeFibonacci(n) {
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+
+    let previous = 0;
+    let current = 1;
+    for ( let i = n; i > 1; i--) {
+        let next = previous + current;
+        previous = current;
+        current = next;
+    }
+    return current;
+}
+
+// Recursion
+
+function recursiveFibonacci(n) {
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+    return recursiveFibonacci(n - 2) + recursiveFibonacci(n - 1);   
+}
