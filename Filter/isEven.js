59da409bc1d3c8e6c0b921a280228cbab271b44a
Patryk <patrykkonior4@gmail.com>
2023-12-17 12:46:31 +0100

    Filter practice

---
+++ Filter/isEven.js
@@ -0,0 +1,12 @@
+wholes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+function filter(predicateFn, array) {
+    if (length(array) === 0) return [];
+    const firstItem = head(array);
+    const filteredFirst = predicateFn(firstItem) ? [firstItem] : [];
+    return concat(filteredFirst, filter(predicateFn, tail(array)));
+  }
+
+function isEven(n) {
+    return n % 2 === 0;
+}
