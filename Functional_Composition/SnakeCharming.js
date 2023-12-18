1b6352cae6560341cfb37daaca50b9d03ff9adc7
Patryk <patrykkonior4@gmail.com>
2023-12-18 17:08:07 +0100

    desnake function

---
+++ Functional_Composition/SnakeCharming.js
@@ -0,0 +1,4 @@
+// Takes a "snake_case_string" and returns a split array of the words, e.g. ["snake", "case", "string"]
+function desnake(snake_case_string) {
+    return snake_case_string.split('_');
+  }
