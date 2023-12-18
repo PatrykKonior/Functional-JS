5af07da8ffc0cd47e87a3241365be5bb167b2183
Patryk <patrykkonior4@gmail.com>
2023-12-18 17:10:05 +0100

    Capitalize first letter function

--- Functional_Composition/SnakeCharming.js
+++ Functional_Composition/SnakeCharming.js
@@ -2,3 +2,8 @@
 function desnake(snake_case_string) {
     return snake_case_string.split('_');
   }
+
+// Takes a "string" and returns a string with the first letter capitalized, e.g. "String"
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.substr(1).toLowerCase();
+  }
